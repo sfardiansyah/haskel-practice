4493cfb236775c87e4b4a10169968335dcdc95ab
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2019-10-18 22:35:29 +0700

    Change 'and' to &&

--- kuis1.hs
+++ kuis1.hs
@@ -1,4 +1,4 @@
 -- KPK Jawaban
-kpk a b = head [x | x <- [a..a*b], y <- [b..a*b],   (((x `mod` a) == 0) and
-                                                    ((y `mod` b) == 0)) and 
+kpk a b = head [x | x <- [a..a*b], y <- [b..a*b],   (((x `mod` a) == 0) &&
+                                                    ((y `mod` b) == 0)) && 
                                                     (x == y)]
