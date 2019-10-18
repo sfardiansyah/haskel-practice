abe8853cfcffd8e0d2cca5b37e757a03c358841e
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2019-10-18 23:12:40 +0700

    Change '/' to `div`

--- kuis1.hs
+++ kuis1.hs
@@ -6,9 +6,7 @@
 
 -- KPK Logic
 -- kpk a b * fpb a b = a*b
-fpb :: Int -> Int -> Int
 fpb 0 b = b
 fpb a 0 = a
 fpb a b = fpb b (a `mod` b)
-kpk :: Int -> Int -> Int
-kpk a b = (a * b) / (fpb a b)
+kpk a b = (a * b) `div` (fpb a b)
