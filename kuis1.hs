9512a283de599bb26f66d97cc06aacee29d42e5a
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2019-10-18 22:54:37 +0700

    Count KPK without list comprehension

--- kuis1.hs
+++ kuis1.hs
@@ -1,4 +1,12 @@
+-- 1. KPK
 -- KPK Jawaban
-kpk a b = head [x | x <- [a..a*b], y <- [b..a*b],   (((x `mod` a) == 0) &&
-                                                    ((y `mod` b) == 0)) && 
-                                                    (x == y)]
+-- kpk a b = head [x | x <- [a..a*b], y <- [b..a*b],   (((x `mod` a) == 0) &&
+--                                                     ((y `mod` b) == 0)) && 
+--                                                     (x == y)]
+
+-- KPK Logic
+-- kpk a b * fpb a b = a*b
+fpb 0 b = b
+fpb a 0 = a
+fpb a b = fpb b (a `mod` b)
+kpk a b = (a * b) / (fpb a b)
