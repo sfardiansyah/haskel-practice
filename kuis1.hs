143c506472ef889e2b89adcbb1e416bc738378c0
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2019-10-18 23:58:07 +0700

    Add question number 2

--- kuis1.hs
+++ kuis1.hs
@@ -4,14 +4,18 @@ kpk a b = head [x | x <- [a..a*b], y <- [b..a*b],   (((x `mod` a) == 0) &&
                                                     ((y `mod` b) == 0)) && 
                                                     (x == y)]
 
+
 -- KPK Logic
-kpk a b * fpb a b = a*b
+-- kpk a b * fpb a b = a*b
 fpb 0 b = b
 fpb a 0 = a
 fpb a b = fpb b (a `mod` b)
 kpk a b = (a * b) `div` (fpb a b)
 
+
 -- KPK Using map in list comprehension
 kpk a b = head [z | z <- (map (*a) [1..]), z `mod` b == 0]
 
--- 2. 
+
+-- 2. (x, y) pair
+xypair = [(x,y) | x <- [1..3], y <- [1..(2*x)]]
